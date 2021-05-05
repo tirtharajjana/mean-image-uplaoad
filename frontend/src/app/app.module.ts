40c4413845b55ac51f152c66ef66d5cbfd2ff1a5
Tirtharaj <tirtharajjana615@gmail.com>
2021-05-05 16:45:56 +0530

    create components and services

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,27 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule}  from '@angular/common/http';
+
+import { AllProfileComponent } from './components/all-profile/all-profile.component';
+import { CreateProfileComponent } from './components/create-profile/create-profile.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProfileComponent,
+    AllProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/all-profile/all-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-profile',
+  templateUrl: './all-profile.component.html',
+  styleUrls: ['./all-profile.component.css']
+})
+export class AllProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
