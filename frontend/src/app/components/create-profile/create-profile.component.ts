4d863952da6af36e3b9ae27c2f2a6d027e25ee07
Tirtharaj <tirtharajjana615@gmail.com>
2021-05-07 16:16:09 +0530

    create basic frontend

--- frontend/src/app/components/create-profile/create-profile.component.ts
+++ frontend/src/app/components/create-profile/create-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { Profile } from '../../models/Profile'
 
 @Component({
   selector: 'app-create-profile',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-profile.component.css']
 })
 export class CreateProfileComponent implements OnInit {
+  form!:FormGroup ;
+  profile: Profile | undefined;
+  imageData:string |undefined;
+  
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form=new FormGroup({
+        name:new FormControl(null),
+        image:new FormControl(null)
+    })
+  }
+
+  onFileSelect(event:Event){
+    console.log("file Selected");
+    
+  }
+
+  onSubmit(){
+    console.log("Submit Profile");
+    
   }
 
 }
