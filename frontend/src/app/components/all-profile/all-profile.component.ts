361bb6ea174d554c698cf821a3e95ed7a59d549d
Tirtharaj <tirtharajjana615@gmail.com>
2021-05-08 19:04:53 +0530

    show all images

--- frontend/src/app/components/all-profile/all-profile.component.ts
+++ frontend/src/app/components/all-profile/all-profile.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Profile } from './../../models/Profile';
+import { ProfileService } from './../../services/profile.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-all-profile',
   templateUrl: './all-profile.component.html',
   styleUrls: ['./all-profile.component.css']
 })
-export class AllProfileComponent implements OnInit {
+export class AllProfileComponent implements OnInit, OnDestroy {
+  profiles: Profile[] = [];
+  private profileSubscription: Subscription;
 
-  constructor() { }
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
+    this.profileService.getProfiles();
+    this.profileSubscription = this.profileService.getProfilesStream().subscribe((profiles: Profile[]) => {
+      this.profiles = profiles;
+    })
+  }
+
+  ngOnDestroy() {
+    this.profileSubscription.unsubscribe();
   }
 
 }
--- frontend/src/app/services/profile.service.ts
+++ frontend/src/app/services/profile.service.ts
@@ -1,9 +1,32 @@
+import { Profile } from './../models/Profile';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import {map } from 'rxjs/operators'
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private profiles: Profile[] = [];
+  private profiles$=new Subject<Profile[]>();
+  readonly url = "http://localhost:3000/api/profiles";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProfiles() {
+    this.http.get<{ profiles: Profile[] }>(this.url).pipe(
+      map((profileData)=>{
+        return profileData.profiles;
+      })
+    ).subscribe((profiles)=>{
+      this.profiles=profiles;
+      this.profiles$.next(this.profiles);
+    })
+  }
+
+  getProfilesStream(){
+    return this.profiles$.asObservable();
+  }
 }
