8d9f3c57041f47d73555335d40495deeb4585726
Tirtharaj <tirtharajjana615@gmail.com>
2021-05-10 19:17:16 +0530

    create database and it's schema

--- backend/routes/profiles.js
+++ backend/routes/profiles.js
@@ -3,3 +3,10 @@ const express=require("express");
 //controller
 
 //storage
+const router =express.Router();
+
+// router.get('/',)
+
+router.post('/',storage,profilesController.postProfile);
+
+module.exports=router;
