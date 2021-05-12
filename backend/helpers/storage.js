8ab3a3a5b52bf1ec09a4c853774acd85811c7abf
Tirtharaj <tirtharajjana615@gmail.com>
2021-05-12 15:43:43 +0530

    create multer function part 2

--- backend/helpers/storage.js
+++ backend/helpers/storage.js
@@ -16,5 +16,9 @@ const diskStorage = multer.diskStorage(
 
 const fileFilter = (req, file, cb) => {
     const allowedMimeTypes = ["image/png", "image/jpeg", "image/jpg"];
-
+    allowedMimeTypes.includes(file.mimetype) ? cb(null, true) : cb(null, false)
 }
+
+const storage = multer({ storage: diskStorage, fileFilter: fileFilter }).single('image');
+
+module.exports = storage;
